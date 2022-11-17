9aee2031d7bc62faa683c4b5235358bbc92be2c7
DelvonRH <delvonrowley@gmail.com>
2022-11-17 18:00:15 -0500

    first commit

---
+++ src/main/java/com/tts/Authentication/AuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.tts.Authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationApplication.class, args);
+	}
+
+}
